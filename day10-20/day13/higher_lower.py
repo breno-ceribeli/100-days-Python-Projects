524d417841b6b59074ad4cd2a95a2696d43fe7db
breno-ceribeli <ceribelibreno@gmail.com>
2024-07-26 22:31:58 -0300

    Function and variable improvement

--- day10-20/day13/higher_lower.py
+++ day10-20/day13/higher_lower.py
@@ -10,20 +10,17 @@ def clear_console():
         os.system('clear')
 
 def newOption(previous, lastRound):
-    """Generates a new option preventing it from being the same as the last two and returns a list containing a option description and it's followers."""
+    """Generates a new option preventing it from being the same as the last two and returns the option description and it's followers."""
     optionDescription = previous
     while optionDescription == previous or optionDescription == lastRound:
         option = choice(data)
         optionDescription = f"{prefix_B}{option['name']}, a {option['description']}, from {option['country']}."
-    return [optionDescription, option['follower_count']]
+    return (optionDescription, option['follower_count'])
 
 def changePrefix(option):
-    """Return the option with the right prefix."""
-    if option.startswith(prefix_B):
-        prefixedOption = option[len(prefix_B):]
-        prefixedOption = prefix_A + prefixedOption
-    else:
-        prefixedOption = prefix_B + option
+    """Return the option changing the prefix B to prefix A."""
+    prefixedOption = option[len(prefix_B):]
+    prefixedOption = prefix_A + prefixedOption
     return prefixedOption
 
 prefix_A = "Compare A: "
@@ -34,24 +31,19 @@ def higherOrLower():
     scoreMessage = ""
     previousOption = ""
     lastRoundOption = ""
-    firstOption = newOption(previousOption, lastRoundOption)
+    FODescription, FOFollowers = newOption(previousOption, lastRoundOption)
     shouldContinue = True
     print(logo)
     while shouldContinue:
         if scoreMessage != "":
             print(scoreMessage)
-        FODescription = firstOption[0]
-        FOFollowers = firstOption[1]
         prefixedFO = changePrefix(FODescription)
         print(prefixedFO)
         lastRoundOption = previousOption
         previousOption = FODescription
         print(vs)
-        secondOption = newOption(previousOption, lastRoundOption)
-        SODescription = secondOption[0]
-        SOFollowers = secondOption[1]
+        SODescription, SOFollowers = newOption(previousOption, lastRoundOption)
         print(SODescription)
-        firstOption = secondOption
         userChoice = ""
         while userChoice not in ('A', 'B'):
             userChoice = input("Who has more followers? Type 'A' or 'B': ").upper()
@@ -65,6 +57,8 @@ def higherOrLower():
             clear_console()
             print(logo)
             print(f"Sorry, that's wrong. Final score: {score}")
+        FODescription = SODescription
+        FOFollowers = SOFollowers
 higherOrLower()
 
 while input("Do you want to play again ? Type 'y': ").lower() == "y":
