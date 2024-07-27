023be49559efc02316ebf3394cb7b760d6434b83
breno-ceribeli <ceribelibreno@gmail.com>
2024-07-26 22:56:36 -0300

    snake_case usage

--- day10-20/day13/higher_lower.py
+++ day10-20/day13/higher_lower.py
@@ -9,60 +9,60 @@ def clear_console():
     else:
         os.system('clear')
 
-def newOption(previous, lastRound):
+def new_option(previous, last_round):
     """Generates a new option preventing it from being the same as the last two and returns the option description and it's followers."""
-    optionDescription = previous
-    while optionDescription == previous or optionDescription == lastRound:
+    option_description = previous
+    while option_description == previous or option_description == last_round:
         option = choice(data)
-        optionDescription = f"{prefix_B}{option['name']}, a {option['description']}, from {option['country']}."
-    return (optionDescription, option['follower_count'])
+        option_description = f"{prefix_B}{option['name']}, a {option['description']}, from {option['country']}."
+    return (option_description, option['follower_count'])
 
-def changePrefix(option):
+def change_prefix(option):
     """Return the option changing the prefix B to prefix A."""
-    prefixedOption = option[len(prefix_B):]
-    prefixedOption = prefix_A + prefixedOption
-    return prefixedOption
+    prefixed_option = option[len(prefix_B):]
+    prefixed_option = prefix_A + prefixed_option
+    return prefixed_option
 
 prefix_A = "Compare A: "
 prefix_B = "Against B: "
 
-def higherOrLower():
+def higher_or_lower():
     score = 0
-    scoreMessage = ""
-    previousOption = ""
-    lastRoundOption = ""
-    FODescription, FOFollowers = newOption(previousOption, lastRoundOption)
-    shouldContinue = True
+    score_message = ""
+    previous_option = ""
+    last_round_option = ""
+    first_option_description, first_option_followers = new_option(previous_option, last_round_option)
+    should_continue = True
     print(logo)
-    while shouldContinue:
-        if scoreMessage != "":
-            print(scoreMessage)
-        prefixedFO = changePrefix(FODescription)
-        print(prefixedFO)
-        lastRoundOption = previousOption
-        previousOption = FODescription
+    while should_continue:
+        if score_message != "":
+            print(score_message)
+        prefixed_first_option = change_prefix(first_option_description)
+        print(prefixed_first_option)
+        last_round_option = previous_option
+        previous_option = first_option_description
         print(vs)
-        SODescription, SOFollowers = newOption(previousOption, lastRoundOption)
-        print(SODescription)
-        userChoice = ""
-        while userChoice not in ('A', 'B'):
-            userChoice = input("Who has more followers? Type 'A' or 'B': ").upper()
-        if (userChoice == 'A' and FOFollowers > SOFollowers) or (userChoice == 'B' and SOFollowers > FOFollowers):
+        second_option_description, second_option_followers = new_option(previous_option, last_round_option)
+        print(second_option_description)
+        user_guess = ""
+        while user_guess not in ('A', 'B'):
+            user_guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+        if (user_guess == 'A' and first_option_followers > second_option_followers) or (user_guess == 'B' and second_option_followers > first_option_followers):
             score += 1
             clear_console()
             print(logo)
             print(f"You're right! Current score: {score}")
         else:
-            shouldContinue = False
+            should_continue = False
             clear_console()
             print(logo)
             print(f"Sorry, that's wrong. Final score: {score}")
-        FODescription = SODescription
-        FOFollowers = SOFollowers
-higherOrLower()
+        first_option_description = second_option_description
+        first_option_followers = second_option_followers
+higher_or_lower()
 
 while input("Do you want to play again ? Type 'y': ").lower() == "y":
     clear_console()
-    higherOrLower()
+    higher_or_lower()
 
 #Old game data
